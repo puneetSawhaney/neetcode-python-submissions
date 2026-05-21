5ed2ba660fc236691575d31ee7eabb8fee43ac85
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2026-05-21 23:19:17 +0530

    Create StringMethods.py

---
+++ StringMethods.py
@@ -0,0 +1,44 @@
+#String Methods
+
+st =  "abc"
+
+print (st.isalpha())
+
+st2= "abc3"
+
+print(st2.isalpha())
+
+st3 = "12334"
+print(st3.isdigit())
+
+st4 = "ab154364"
+print(st4.isdigit())
+
+
+st =  "abc"
+
+print (st.islower())
+
+st2= "ABC"
+
+print(st2.islower())
+
+print(st.isupper())
+
+print(st2.isupper())
+
+print(st2.lower())
+print(st.upper())
+
+#lstrip() // remove spaces from left side
+#rstrip // remove spaces from right side
+
+# for all above print statements I have below output from print statements
+
+'''
+True
+False
+True
+False
+True
+False
